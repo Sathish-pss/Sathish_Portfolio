a2ba947cc35aa3ff9d5addaba4fe18a1c530d292
Sathish-pss <sathishganesan.official@gmail.com>
2024-06-07 18:21:52 +0530

    Update Portfolio
    => Completed the Portfolio for my personal

---
+++ sathish_portfolio/data/index.ts
@@ -0,0 +1,134 @@
+// Navbar Section
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a E-Commerce website inspired by Amazon",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+// Deploy the app, and add it here the link
+export const projects = [
+  {
+    id: 1,
+    title: "NexCall - A Full Stack Video Conferencing App",
+    des: "Simplify your video conferencing experience with NexCall. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Sathish was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sathish's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sathish is the ideal partner.",
+    name: "Sathish",
+    title: "Software Developer",
+  },
+];
+
+// Companies data here
+export const companies = [
+  {
+    id: 1,
+    name: "Phantom Solutions",
+    img: "/N-logo.png",
+    nameImg: "/phantom-logo.png",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "React JS Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Software Developer",
+    desc: "Developed and maintained a highly customizable SaaS multi-tenant platform using React.js, Next.js allowing multiple clients to operate with the same application while keeping data and configurations isolated",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
